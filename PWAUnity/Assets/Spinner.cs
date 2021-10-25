b9f838010ecaf2c72974a0bf3de345a03c104ed1
Joe Shochet <shochet@gmail.com>
2021-10-25 10:01:28 -0700

    Added Unity project

---
+++ PWAUnity/Assets/Spinner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public float rate = 10f;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(rate * Time.deltaTime, rate * 2 * Time.deltaTime, rate * 3 * Time.deltaTime);
+    }
+}
